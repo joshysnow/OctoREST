017d3bf28e3307082c4280cdd1c607a4ea597a1b
Ellis Calderbank <ecalder95@live.co.uk>
2017-10-31 14:18:22 +0000

    Implemented POST. Added a user to a JSON

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
-var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000;
-  var fs = require("fs");
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
+var port = process.env.PORT || 3000;
+var fs = require("fs");
 
-  app.get('/users', function(req, res) {
-    fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err,data){
+app.use(bodyParser.json());
 
-      console.log(data);
-      res.end(data);
-    });
+app.get('/users', function(req, res) {
+  fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err,data){
+    console.log(data);
+    res.end(data);
   });
-  app.get('/', (req, res) => {
-    res.send ('Welcome to our API');
-  });
-  app.listen(port);
+});
 
-  console.log('OctoREST started on: ' + port);
+app.get('/', (req, res) => {
+  res.send ('Welcome to our API');
+});
+
+app.post('/users', function (req, res){
+  const inputData = req.body;
+  let id;
+
+  fs.readFile('users.json', function (err, data){
+      var json = JSON.parse(data);
+      id = Date.now();
+      console.log(json);
+      json[id] = inputData;
+
+      fs.writeFile("users.json", JSON.stringify(json));
+  })
+
+  res.send({'id': id});
+});
+
+app.listen(port);
+
+console.log('OctoREST started on: ' + port);
